fdd988faccf949819869f143d5a7fd922ff22b5a
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-11-13 23:04:05 +0530

    Setup navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,10 +1,12 @@
+import "./style.css";
+
 export const Navbar = (props) => {
   return (
     <nav>
-      <div>
+      <div className="nav-links">
         <a href="/">Home</a>
       </div>
-      <div>
+      <div className="nav-links">
         <a href="/planets">Planets</a>
       </div>
     </nav>
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "./Navbar";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <Navbar />;
 }
