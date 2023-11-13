9d4c8156ca3cecced67fbcd4eadbe0e960bdc043
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-11-13 22:37:07 +0530

    init commit

---
+++ src/Navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = (props) => {
+  return (
+    <nav>
+      <div>
+        <a href="/">Home</a>
+      </div>
+      <div>
+        <a href="/planets">Planets</a>
+      </div>
+    </nav>
+  );
+};
