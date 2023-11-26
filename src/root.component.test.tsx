d6b7baea07ccee63df7bbad3f2d888c05a79d7b1
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-11-26 21:30:41 +0530

    Default React Import

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import Root from "./root.component";
+import React from "react";
 
 describe("Root component", () => {
   it("should be in the document", () => {
